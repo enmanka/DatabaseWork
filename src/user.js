7ce41941a2234f12e08303d9392698e4fe18aeb4
qqmmyy <164297647+qqmmyy@users.noreply.github.com>
2024-12-03 21:18:09 +0800

    Update user.js

--- src/user.js
+++ src/user.js
@@ -3,12 +3,18 @@ import request from '@/utils/request'
 // 用户主页
 export function getInfoByName(username, page, size) {
   return request({
-    url: '/ums/user/' + username,
+    url: `/ums/user/username/${username}`,
     method: 'get',
     params: {
       pageNo: page,
       size: size
     }
+  })
+}
+export function getInfoById(id){
+  return request({
+    url:`/ums/user/id/${id}`,
+    method:'get'
   })
 }
 
